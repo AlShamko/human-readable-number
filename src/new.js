fb85db8a880060371f641e9d0500ef18848851c5
Aleksandr Shamko <shamkoag@gmail.com>
2024-08-06 18:31:57 +0200

    add new function and fix bug

---
+++ src/new.js
@@ -0,0 +1,96 @@
+function toReadable(number) {
+	const myArrayOne = {
+		0:'zero',
+		1:'one', 
+		2:'two', 
+		3:'three', 
+		4:'four', 
+		5:'five', 
+		6:'six', 
+		7:'seven', 
+		8:'eight', 
+		9:'nine', 
+		10:'ten',
+		11:'eleven',
+		12:'twelve',
+		13:'thirteen',
+		14:'fourteen',
+		15:'fifteen',
+		16:'sixteen',
+		17:'seventeen',
+		18:'eighteen',
+		19:'nineteen',
+	};
+
+	const myArrayF = {
+		0:'zero',
+		1:'one', 
+		2:'two', 
+		3:'three', 
+		4:'four', 
+		5:'five', 
+		6:'six', 
+		7:'seven', 
+		8:'eight', 
+		9:'nine', 
+	};
+
+	const myArrayTwo = {
+		1:'zero',
+		0:'zero',
+		20:'twenty',
+		30:'thirty', 
+		40:'forty', 
+		50:'fifty', 
+		60:'sixty', 
+		70:'seventy', 
+		80:'eighty', 
+		90:'ninety', 
+	};
+
+	const myArrayThree = {
+		0:'zero',
+		100:'one hundred',
+		200:'two hundred', 
+		300:'three hundred', 
+		400:'four hundred', 
+		500:'five hundred', 
+		600:'six hundred', 
+		700:'seven hundred', 
+		800:'eight hundred', 
+		900:'nine hundred', 
+	};
+
+	if(number < 20) {
+		return myArrayOne[number];
+	}
+
+	if(number < 100) {
+		const dvoiki = Array.from(String(number), Number);
+		const odinki = Array.from(String(number), Number);
+		const a = myArrayTwo[dvoiki[0] * 10];
+		const b = myArrayF[odinki[1]] === 'zero' ? "" : myArrayF[odinki[1]];
+		const result = a + ' ' + b;
+		return result.replace(/ +/g, ' ').trim()
+	}
+
+	if(number < 120) {
+		const troiki = Array.from(String(number), Number);
+		const z = myArrayThree[troiki[0] * 100];
+		const result = (z + ' ' + myArrayOne[number - 100]);
+		return result
+	}
+
+	if(number < 200) {
+		const troiki = Array.from(String(number), Number);
+		const dvoiki = Array.from(String(number), Number);
+		const odinki = Array.from(String(number), Number);
+		const z = myArrayThree[troiki[0] * 100];
+		const a = myArrayTwo[dvoiki[1] * 10] === 'zero' ? "" : myArrayTwo[dvoiki[1] * 10];
+		const b = myArrayF[odinki[2]] === 'zero' ? "" : myArrayF[odinki[2]];
+		const result = (z + ' ' + a + ' ' + b);
+		return result.replace(/ +/g, ' ').trim()
+	}  
+}
+
+console.log(toReadable(119));
